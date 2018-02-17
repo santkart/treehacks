ac4d084bf148996cd409885e12b82c586258f700
anniex1 <32438139+anniex1@users.noreply.github.com>
2018-02-17 11:38:48 -0800

    Add files via upload

---
+++ quizLib.java
@@ -0,0 +1,17 @@
+package com.example.annie.treehacks18;
+
+/**
+ * Created by Annie on 2/17/2018.
+ */
+
+public class quizLib {
+    /*
+    private String mQuestions[] = {
+            "Q1", "Q2", "Q3"
+    };
+
+    private String mChoices [][] = {
+            {"a11", "a12", "a13"}, {},
+
+    }*/
+}
